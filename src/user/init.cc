11e713809c40d82ffbb6d1584ffe7b010c4d475e
Joseph Arhar <josepharhar@gmail.com>
2017-12-13 14:35:29 -0800

    some user class testing in init

--- src/user/init.cc
+++ src/user/init.cc
@@ -6,15 +6,62 @@
 #include "clone.h"
 #include "getpid.h"
 
+// TODO the first piece of code in this file is what gets run by
+// exec(), make it always look for main() somehow instead
+void proc_testing();
+void class_testing();
+int main() {
+  Puts("Hello from init\n");
+  //proc_testing();
+  class_testing();
+
+  Puts("\ninit process ending\n");
+  // TODO ProcExit();
+}
+
+
+class Class {
+ public:
+  Class() {
+    printu("Class::Class()\n");
+  }
+  ~Class() {
+    printu("Class::~Class()\n");
+  }
+  Class(const Class& other) {
+    printu("Class::Class(const Class&)\n");
+  }
+  Class& operator=(const Class& other) {
+    printu("Class::operator=(const Class&)\n");
+    return *this;
+  }
+
+  virtual void PrintClassName() {
+    printu("PrintClassName Class\n");
+  }
+};
+
+class SubClass : public Class {
+ public:
+  void PrintClassName() override {
+    printu("PrintClassName SubClass\n");
+  }
+};
+
+void class_testing() {
+  printu("\n");
+
+  SubClass asdf;
+  asdf.PrintClassName();
+}
+
+
 static void NewProc();
 extern char new_stack[];
 
 //static int asdf = 130;
 extern int asdf;
-
-// TODO the first piece of code in this file is what gets run by
-// exec(), make it always look for main() somehow instead
-int main() {
+void proc_testing() {
   Puts("Hello from init\n");
   printu("main() pid: %d\n", getpid());
 
