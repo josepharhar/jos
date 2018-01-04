d1dac6cbc67bda4e1cc00b8dbab1684ff46ac0cb
Joseph Arhar <josepharhar@gmail.com>
2018-01-03 22:34:16 -0800

    finished writing jbuffer, tests fail

--- src/shared/jbuffer.h
+++ src/shared/jbuffer.h
@@ -1,13 +1,20 @@
 #ifndef SHARED_JBUFFER_H_
 #define SHARED_JBUFFER_H_
 
+#include "string.h"
+
 namespace stdj {
 
+// Stores |size| "T"s in a circular buffer
 template <typename T>
 class Buffer {
  public:
   Buffer(uint64_t size)
-      : size_(size), read_index_(0), write_index_(1), buffer_(new T[size]) {}
+      : size_(size),
+        read_index_(0),
+        write_index_(0),
+        buffer_(new T[size]),
+        is_full_(false) {}
   ~Buffer() { delete[] buffer_; }
 
   Buffer(const Buffer& other) = delete;
@@ -16,19 +23,87 @@ class Buffer {
   Buffer(Buffer&& other) = delete;
   Buffer& operator=(Buffer&& other) = delete;
 
-  void Write(const T* source_buffer, uint64_t write_size) {
+  // Returns number of entries written
+  uint64_t Write(const T* source_buffer, uint64_t write_size) {
+    if (SizeRemaining() < write_size) {
+      write_size = SizeRemaining();
+    }
+
+    // check to see if we have to do multiple copies
+    if (write_index_ + write_size >= Size()) {
+      // multiple copies
+      uint64_t first_copy_size = Size() - write_index_;
+      memcpy(buffer_ + write_index_, source_buffer, first_copy_size);
+      memcpy(buffer_, source_buffer + first_copy_size,
+             write_size - first_copy_size);
+      write_index_ = write_size - first_copy_size;
+
+    } else {
+      // one copy
+      memcpy(buffer_ + write_index_, source_buffer, write_size);
+      write_index_ = (write_index_ + write_size) % size_;
+    }
+
+    // update is_full_
+    if (write_size > 0 && write_index_ == read_index_) {
+      is_full_ = true;
+    } else {
+      is_full_ = false;
+    }
+
+    return write_size;
   }
 
-  void Read(T* dest_buffer, uint64_t read_size) {
+  // Returns number of entries read
+  uint64_t Read(T* dest_buffer, uint64_t read_size) {
+    if (SizeUsed() > read_size) {
+      read_size = SizeUsed();
+    }
+
+    if (read_index_ + read_size >= Size()) {
+      // two copies
+      uint64_t first_copy_size = Size() - read_index_;
+      memcpy(dest_buffer, buffer_ + read_index_, first_copy_size);
+      memcpy(dest_buffer + first_copy_size, buffer_,
+             (read_size - first_copy_size));
+      read_index_ = read_size - first_copy_size;
+
+    } else {
+      // one copy
+      memcpy(dest_buffer, buffer_ + read_index_, read_size);
+      read_index_ = (read_index_ + read_size) % size_;
+    }
+
+    // update is_full_
+    if (read_size > 0 && read_index_ == write_index_) {
+      is_full_ = true;
+    } else {
+      is_full_ = false;
+    }
+
+    return read_size;
   }
 
   uint64_t Size() const { return size_; }
+
+  uint64_t SizeUsed() const {
+    if (read_index_ == write_index_) {
+      return is_full_ ? size_ : 0;
+    } else if (read_index_ > write_index_) {
+      return read_index_ - write_index_;
+    } else {
+      return (Size() - read_index_) + write_index_;
+    }
+  }
+
+  uint64_t SizeRemaining() const { return Size() - SizeUsed(); }
 
  private:
   T* buffer_;
   uint64_t size_;
   uint64_t read_index_;
   uint64_t write_index_;
+  bool is_full_;
 };
 
 }  // namespace stdj
--- src/test/jbuffer_test.cc
+++ src/test/jbuffer_test.cc
@@ -2,23 +2,33 @@
 
 #include "test.h"
 
+#include "shared/jbuffer.h"
+
 int main(int argc, char** argv) {
-  std::string string_one = "i am ";
-  std::string string_two = "sofa king";
-  std::string string_three = "you say funny thing";
+  std::string string_one = "1234";
+  std::string string_two = "7654321";
   std::string read_string;
   std::string expected_string;
 
-  {
-    stdj::Buffer<char> buffer(20);
-    buffer.Write(string_one.data(), string_one.size());
-    buffer.Write(string_two.data(), string_two.size());
+  stdj::Buffer<char> buffer(20);
+  ASSERT_EQ((uint64_t)0, buffer.SizeUsed());
+
+  for (int i = 0; i < 30; i++) {
+    ASSERT_EQ((uint64_t)string_one.size(),
+              buffer.Write(string_one.data(), string_one.size()));
+    ASSERT_EQ((uint64_t)string_two.size(),
+              buffer.Write(string_two.data(), string_two.size()));
     expected_string = string_one + string_two;
+
+    ASSERT_EQ((uint64_t)(string_one.size() + string_two.size()),
+              buffer.SizeUsed());
 
     read_string.clear();
     read_string.resize(expected_string.size());
-    buffer.Read(read_string.data(), read_string.size());
+    ASSERT_EQ((uint64_t)read_string.size(),
+              buffer.Read((char*)read_string.data(), read_string.size()));
 
+    ASSERT_EQ((uint64_t)0, buffer.SizeUsed());
     ASSERT_EQ(expected_string, read_string);
   }
 }
