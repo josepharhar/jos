c5aa8620fd804278b18949b497565c6afa5bf956
Joseph Arhar <josepharhar@gmail.com>
2018-02-25 22:16:09 -0800

    redoing vfs is going to be hard and i should wait until after other milestones

--- src/kernel/files.cc
+++ src/kernel/files.cc
@@ -4,26 +4,22 @@
 #include "ref_counted.h"
 
 // disregards paths, directories, just finds the name
-Inode* FindFile(Inode* inode, char* filename) {
-  if (inode->IsDirectory()) {
-    LinkedList<Inode*>* sub_inodes = inode->ReadDir();
+Inode FindFile(Inode inode, char* filename) {
+  if (inode.IsDirectory()) {
+    stdj::Array<Inode> sub_inodes = inode.ReadDir();
 
-    //RefCounted<Iterator<Inode*>> iterator(sub_inodes->GetIterator());
-    Iterator<Inode*>* iterator = sub_inodes->GetIterator();
-    while (iterator->HasNext()) {
-      Inode* sub_inode = iterator->Next();
-      if (strcmp(sub_inode->GetName(), "..") && strcmp(sub_inode->GetName(), ".")) {
-        Inode* sub_result = FindFile(sub_inode, filename);
+    for (int i = 0; i < sub_inodes.Size(); i++) {
+      Inode sub_inode = sub_inodes[i];
+      if (strcmp(sub_inode.GetName(), "..") && strcmp(sub_inode.GetName(), ".")) {
+        Inode sub_result = FindFile(sub_inode, filename);
         if (sub_result) {
           return sub_result;
         }
       }
-
-      delete sub_inode;
     }
     return 0;
   } else {
-    if (!strcmp(inode->GetName(), filename)) {
+    if (!strcmp(inode.GetName(), filename)) {
       return inode;
     } else {
       return 0;
