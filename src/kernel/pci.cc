11931c8145dcb9dbfb5383cf6d6f6a5b3adf8f0c
Joseph Arhar <josepharhar@gmail.com>
2018-05-11 16:13:15 -0700

    prints out device info

--- src/kernel/pci.cc
+++ src/kernel/pci.cc
@@ -1,11 +1,12 @@
 #include "pci.h"
 
 #include "asm.h"
+#include "printk.h"
 
 #define CONFIG_ADDRESS 0xCF8
 #define CONFIG_DATA 0xCFC
 
-struct Config {
+/*struct Config {
   uint32_t enable : 1;
   uint32_t reserved : 7;
   uint32_t bus_number : 8;
@@ -13,59 +14,77 @@ struct Config {
   uint32_t function_number : 3;
   uint32_t register_number : 6;
   uint32_t zero : 2;
-} __attribute__((packed));
+} __attribute__((packed));*/
 
 uint16_t pciConfigReadWord(uint8_t bus,
-                           uint8_t slot,
+                           uint8_t device,
                            uint8_t func,
                            uint8_t offset) {
   uint32_t address;
   uint32_t lbus = (uint32_t)bus;
-  uint32_t lslot = (uint32_t)slot;
+  uint32_t ldevice = (uint32_t)device;
   uint32_t lfunc = (uint32_t)func;
 
   /* create configuration address as per Figure 1 */
-  address = (uint32_t)((lbus << 16) | (lslot << 11) | (lfunc << 8) |
+  address = (uint32_t)((lbus << 16) | (ldevice << 11) | (lfunc << 8) |
                        (offset & 0xfc) | ((uint32_t)0x80000000));
 
   /* write out the address */
-  sysOutLong(0xCF8, address);
+  outl(0xCF8, address);
   /* read in the data */
   /* (offset & 2) * 8) = 0 will choose the first word of the 32 bits register */
-  return (uint16_t)((sysInLong(0xCFC) >> ((offset & 2) * 8)) & 0xffff);
+  return (uint16_t)((inl(0xCFC) >> ((offset & 2) * 8)) & 0xffff);
 }
 
-uint16_t pciCheckVendor(uint8_t bus, uint8_t slot) {
-  uint16_t vendor, device;
+uint16_t pciCheckVendor(uint8_t bus, uint8_t device) {
+  uint16_t vendor_id, device_id;
   /* try and read the first configuration register. Since there are no */
   /* vendors that == 0xFFFF, it must be a non-existent device. */
-  vendor = pciConfigReadWord(bus, slot, 0, 0);
-  if (vendor != 0xFFFF) {
-    device = pciConfigReadWord(bus, slot, 0, 2);
-    // TODO
-    //...
+  vendor_id = pciConfigReadWord(bus, device, 0, 0);
+  if (vendor_id != 0xFFFF) {
+    device_id = pciConfigReadWord(bus, device, 0, 2);
+    printk("bus: 0x%X, device: 0x%X, vendor_id: 0x%X, device_id: 0x%X\n", bus,
+           device, vendor_id, device_id);
   }
-  return vendor;
+  return vendor_id;
 }
 
+/*void checkFunction(uint8_t bus, uint8_t device, uint8_t function) {
+  printk("checkFunction bus: %d, device: %d, function: %d\n", bus, device,
+         function);
+}
 void checkDevice(uint8_t bus, uint8_t device) {
   uint8_t function = 0;
 
-  vendorID = getVendorID(bus, device, function);
-  if (vendorID == 0xFFFF)
+  uint16_t vendorID = pciCheckVendor(bus, device, function);
+  if (vendorID == 0xFFFF) {
     return;  // Device doesn't exist
+  }
   checkFunction(bus, device, function);
   headerType = getHeaderType(bus, device, function);
   if ((headerType & 0x80) != 0) {
-    /* It is a multi-function device, so check remaining functions */
+    // It is a multi-function device, so check remaining functions
     for (function = 1; function < 8; function++) {
-      if (getVendorID(bus, device, function) != 0xFFFF) {
+      if (pciCheckVendor(bus, device, function) != 0xFFFF) {
         checkFunction(bus, device, function);
       }
     }
   }
+}*/
+
+void checkAllBuses() {
+  uint16_t bus;
+  uint8_t device;
+
+  for (bus = 0; bus < 256; bus++) {
+    for (device = 0; device < 32; device++) {
+      // checkDevice(bus, device);
+      pciCheckVendor((uint8_t)bus, device);
+    }
+  }
 }
 
-void checkFunction(uint8_t bus, uint8_t device, uint8_t function) {}
-
-void InitPci() {}
+void InitPci() {
+  printk("calling checkAllBuses()\n");
+  checkAllBuses();
+}
--- src/shared/asm.h
+++ src/shared/asm.h
@@ -43,7 +43,7 @@ uint8_t inb(uint16_t port);
 void outw(uint16_t port, uint16_t val);
 uint16_t inw(uint16_t port);
 void outl(uint16_t port, uint32_t val);
-uint32_t inl(uint16_t port, uint32_t val);
+uint32_t inl(uint16_t port);
 void io_wait();
 
 int are_interrupts_enabled();
