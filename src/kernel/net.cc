a3b2e1fd57e478350d2f435f9609e194082866b4
Joseph Arhar <josepharhar@gmail.com>
2018-05-20 16:37:15 -0700

    started on network stack

---
+++ src/kernel/net.cc
@@ -0,0 +1,9 @@
+#include "net.h"
+
+namespace net {
+
+void Init() {
+  pci::DeviceInfo device_info =
+}
+
+}
---
+++ src/kernel/net.h
@@ -0,0 +1,10 @@
+#ifndef KERNEL_NET_H_
+#define KERNEL_NET_H_
+
+namespace net {
+
+void Init();
+
+}
+
+#endif  // KERNEL_NET_H_
